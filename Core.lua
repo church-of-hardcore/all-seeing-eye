c9df9b12f28283b576312513602b1b8bd5475bf6
zdeyn <79044233+zdeyn@users.noreply.github.com>
2021-11-09 18:36:25 +1100

    Add Ace3 and AllSeeingEye
    Very minor config - it's basically a demo to ensure the install worked:
    
    Of note:
    - `/ase` and `/allseeingeye` commands exist
    - All-Seeing Eye config options and profile storage exists

---
+++ Core.lua
@@ -0,0 +1,62 @@
+-- we can mixin more Ace libs here
+AllSeeingEye = LibStub("AceAddon-3.0"):NewAddon("AllSeeingEye", "AceEvent-3.0", "AceConsole-3.0")
+
+local AC = LibStub("AceConfig-3.0")
+local ACD = LibStub("AceConfigDialog-3.0")
+
+function AllSeeingEye:OnInitialize()
+	-- uses the "Default" profile instead of character-specific profiles
+	-- https://www.wowace.com/projects/ace3/pages/api/ace-db-3-0
+	self.db = LibStub("AceDB-3.0"):New("AllSeeingEyeDB", self.defaults, true)
+
+	-- registers an options table and adds it to the Blizzard options window
+	-- https://www.wowace.com/projects/ace3/pages/api/ace-config-registry-3-0
+	AC:RegisterOptionsTable("AllSeeingEye_Options", self.options)
+	self.optionsFrame = ACD:AddToBlizOptions("AllSeeingEye_Options", "All-Seeing Eye (1)")
+
+	-- adds a child options table, in this case our profiles panel
+	local profiles = LibStub("AceDBOptions-3.0"):GetOptionsTable(self.db)
+	AC:RegisterOptionsTable("AllSeeingEye_Profiles", profiles)
+	ACD:AddToBlizOptions("AllSeeingEye_Profiles", "Profiles", "AllSeeingEye_Options")
+
+	-- https://www.wowace.com/projects/ace3/pages/api/ace-console-3-0
+	self:RegisterChatCommand("ase", "SlashCommand")
+	self:RegisterChatCommand("allseeingeye", "SlashCommand")
+
+	self:GetCharacterInfo()
+end
+
+function AllSeeingEye:OnEnable()
+	self:RegisterEvent("PLAYER_STARTED_MOVING")
+	self:RegisterEvent("CHAT_MSG_CHANNEL")
+end
+
+function AllSeeingEye:PLAYER_STARTED_MOVING(event)
+	print(event)
+end
+
+function AllSeeingEye:CHAT_MSG_CHANNEL(event, text, ...)
+	-- prints the whole event payload
+	print(event, text, ...)
+end
+
+function AllSeeingEye:GetCharacterInfo()
+	-- stores character-specific data
+	self.db.char.level = UnitLevel("player")
+end
+
+function AllSeeingEye:SlashCommand(input, editbox)
+	if input == "enable" then
+		self:Enable()
+	elseif input == "disable" then
+		-- unregisters all events and calls AllSeeingEye:OnDisable() if you defined that
+		self:Disable()
+	elseif input == "message" then
+		print("this is our saved message:", self.db.profile.someInput)
+	else
+		self:Print("some useful help message")
+		-- https://github.com/Stanzilla/WoWUIBugs/issues/89
+		InterfaceOptionsFrame_OpenToCategory(self.optionsFrame)
+		InterfaceOptionsFrame_OpenToCategory(self.optionsFrame)
+	end
+end
---
+++ Options.lua
@@ -0,0 +1,81 @@
+AllSeeingEye.defaults = {
+	profile = {
+		someToggle = true,
+		someRange = 7,
+		someInput = "Hello World",
+		someSelect = 2, -- Banana
+	},
+}
+
+-- https://www.wowace.com/projects/ace3/pages/ace-config-3-0-options-tables
+AllSeeingEye.options = {
+	type = "group",
+	name = "AllSeeingEye (2)",
+	handler = AllSeeingEye,
+	args = {
+		someToggle = {
+			type = "toggle",
+			order = 1,
+			name = "a checkbox",
+			desc = "some description",
+			-- inline getter/setter example
+			get = function(info) return AllSeeingEye.db.profile.someToggle end,
+			set = function(info, value) AllSeeingEye.db.profile.someToggle = value end,
+		},
+		someRange = {
+			type = "range",
+			order = 2,
+			name = "a slider",
+			-- this will look for a getter/setter on our handler object
+			get = "GetSomeRange",
+			set = "SetSomeRange",
+			min = 1, max = 10, step = 1,
+		},
+		group1 = {
+			type = "group",
+			order = 3,
+			name = "a group",
+			inline = true,
+			-- getters/setters can be inherited through the table tree
+			get = "GetValue",
+			set = "SetValue",
+			args = {
+				someInput = {
+					type = "input",
+					order = 1,
+					name = "an input box",
+					width = "double",
+				},
+				someDescription = {
+					type = "description",
+					order = 2,
+					name = function() return format("The current time is: |cff71d5ff%s|r", date("%X")) end,
+					fontSize = "large",
+				},
+				someSelect = {
+					type = "select",
+					order = 3,
+					name = "a dropdown",
+					values = {"Apple", "Banana", "Strawberry"},
+				},
+			},
+		},
+	},
+}
+
+function AllSeeingEye:GetSomeRange(info)
+	return self.db.profile.someRange
+end
+
+function AllSeeingEye:SetSomeRange(info, value)
+	self.db.profile.someRange = value
+end
+
+-- https://www.wowace.com/projects/ace3/pages/ace-config-3-0-options-tables#title-4-1
+function AllSeeingEye:GetValue(info)
+	return self.db.profile[info[#info]]
+end
+
+function AllSeeingEye:SetValue(info, value)
+	self.db.profile[info[#info]] = value
+end
